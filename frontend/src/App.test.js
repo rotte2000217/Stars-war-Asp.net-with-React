7910f3ecfa115f82beb82a8dcd070149359fa3e9
sajid <sajid.ali@ibex.co>
2020-03-05 13:12:13 +0500

    Components snapshots are added

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,57 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import App from "./App";
+import Question3 from "./question3";
+import Question4 from "./question4";
 
-it("renders correctly", () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchInlineSnapshot(`
-    <div
-      className="App"
-    >
-      <header
-        className="App-header"
-      >
-        <img
-          alt="logo"
-          className="App-logo"
-          src="Star_Wars_Logo.svg.png"
-        />
-        <h1
-          className="App-title"
-        >
-          Welcome to the prototype interactive Test
-        </h1>
-      </header>
-      <div
-        className="App-Theme"
-      >
-        <h2>
-          Which of all Star Wars movies has the longest opening crawl ?
-        </h2>
-        <h3>
-          
-        </h3>
-        <h2>
-          What character (person) appeared in most of the Star Wars films? 
-        </h2>
-        <h3>
-          
-        </h3>
-        <div
-          className="App-Theme"
-        >
-          <h2>
-            What species appeared in the most  number of Star Wars films? 
-          </h2>
-        </div>
-        <div
-          className="App-Theme"
-        >
-          <h2>
-            What planet in Star Wars universe provided largest number of vehicle pilots?
-          </h2>
-        </div>
-      </div>
-    </div>
-  `);
+describe("App", () => {
+  test("snapshot renders App", () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Question3", () => {
+  test("snapshot renders Question3", () => {
+    const component = renderer.create(<Question3 />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Question4", () => {
+  test("snapshot renders Question4", () => {
+    const component = renderer.create(<Question4 />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
